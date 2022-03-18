59672c4c2eb593f650d74d619a368b749ccabf1e
shohayabt <your_email@example.com>
2022-03-19 02:11:13 +0600

    project complete and i will update  design in future

--- js/app.js
+++ js/app.js
@@ -1,45 +1,65 @@
 
 
 const userCard = document.getElementById('user-card');
+const showError = document.getElementById("error-message");
 
+const errorMessage = (m) => {
+    showError .value = m;
+}
 const loadUser = () =>{
     userCard.innerHTML = '';
     const search = document.querySelector('.search input');
     let userName = search.value;
-    userName = userName.toLowerCase();
-    // if(userName === '' || typeof(userCard) == Number){
-    //     userCard.innerHTML = "Please Enter a valid GitHub User name"
-    // }
-    fetch(`https://api.github.com/users/${userName}`)
-    .then(res => res.json())
-    .then(data => setUsers(data))
+    
+    if(userName ==="" || typeof(userName) == Number ){
+        userCard.style.display = 'none'
+        showError.style.display = 'block'
+        errorMessage("Your have to input something to search. ")
+    }
+    else{
+        showError.style.display = 'none'
+        userName = userName.toLowerCase();
+        fetch(`https://api.github.com/users/${userName}`)
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    }
 }
 const setUsers = (user) =>{
-    // userCard.style.display = "block"
-    // userCard.innerHTML = ''
-    const div = document.createElement('div');
-    div.innerHTML = 
-`
-<div id="user-card">
-    <div class="cover-image">
-        <div class="profile-picture">
-        <img src="${user.avatar_url}">
+    if(user.message === 'Not Found' ){
+        document.querySelector('.search input').value = '';
+        userCard.style.display = 'none'
+        showError.style.display = 'block'
+        errorMessage('404 USER NOT FOUND')
+        setTimeout(() =>{
+            location.reload();
+        },2000)
+    }
+    else{
+        document.querySelector('.search input').value = '';
+        const div = document.createElement('div');
+        div.innerHTML = 
+    `
+    <div id="user-card">
+        <div class="cover-image">
+            <div class="profile-picture">
+            <img src="${user.avatar_url}">
+            </div>
+        </div>
+        <div class="user-information">
+            <h1 class="user-name">${user.name? user.name : user.login}</h1>
+            <small style="display: block;">${user.id}</small>
+            <span><i class="fa-solid fa-location-dot"></i>${user.location ? user.location : "can't find user location"}</span>
+        </div>
+        <div class="bio">
+            <p>${user.bio ? user.bio : "Sorry can't find user bio" }</p>
+        </div>
+        <div class="others-information">
+            <div><small>Public Repos</small><small>${user.public_repos}</small></div>
+            <div><small>followers</small><small>${user.followers}</small></div>
+            <div><small>following</small><small>${user.following}</small></div>
         </div>
     </div>
-    <div class="user-information">
-        <h1 class="user-name">${user.name}</h1>
-        <small style="display: block;">${user.id}</small>
-        <span><i class="fa-solid fa-location-dot"></i>${user.location ? user.location : "can't find user location"}</span>
-    </div>
-    <div class="bio">
-        <p>${user.bio ? user.bio : "Sorry can't find user bio" }</p>
-    </div>
-    <div class="others-information">
-        <div><small>Public Repos</small><small>${user.public_repos}</small></div>
-        <div><small>followers</small><small>${user.followers}</small></div>
-        <div><small>following</small><small>${user.following}</small></div>
-    </div>
-</div>
-`
-userCard.appendChild(div);
+    `
+    userCard.appendChild(div);
+    }
 }
